9ffcf5b4346504ae993799edc690be27d97ba3a0
tienshunlo <tienshunlo@gmail.com>
2014-07-29 23:50:39 +0800

    29July14
    29July14

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+alert("hello!");
 
+$(document).ready(function() {
+  $('.user_profile_btn_left').on('click', function() {
+    $('.user_profile_btn_left').toggleClass('active');
+  })
+})
+
+$(document).ready(function() {
+  $('.user_question_items_btn').on('click', function() {
+    $('.user_question_items_btn').removeClass('active');
+    $(this).addClass('active');
+  });
+});
