db8a0113b0eb913a94491060f69671f32a518d0d
tienshunlo <tienshunlo@gmail.com>
2014-07-30 22:51:24 +0800

    item_left
    item_left

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert("hello!");
+// alert("hello!");
 
 $(document).ready(function() {
   $('.user_profile_btn_left').on('click', function() {
@@ -10,5 +10,34 @@ $(document).ready(function() {
   $('.user_question_items_btn').on('click', function() {
     $('.user_question_items_btn').removeClass('active');
     $(this).addClass('active');
+  });
+});
+
+
+
+
+
+$(document).ready(function() {
+  var btn = $('.hamburger'),
+      content = $('.content'),
+      menu = $('nav');
+  $('.hamburger').on('click', function(event) {
+    event.preventDefault();
+    content.toggleClass('open');
+    if (content.hasClass('open')) {
+      menu.stop().animate({
+        left: 0
+      }, 500);
+      content.stop().animate({
+        marginLeft: 200
+      }, 500);
+    } else {
+      menu.stop().animate({
+        left: -200
+      }, 500);
+      content.stop().animate({
+        marginLeft: 0
+      }, 500);
+    }
   });
 });
