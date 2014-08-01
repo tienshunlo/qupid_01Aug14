0bd6dcc64fa5639e90aced0bb38b62459913df13
tienshunlo <tienshunlo@gmail.com>
2014-08-02 01:33:52 +0800

    hide and show
    hide and show

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // alert("hello!");
 
-$(document).ready(function() {
-  $('.user_profile_btn_left').on('click', function() {
-    $('.user_profile_btn_left').toggleClass('active');
-  })
-})
+//$(document).ready(function() {
+ // $('.user_profile_btn_left').on('click', function() {
+//    $('.user_profile_btn_left').toggleClass('active');
+//  })
+//})
 
-$(document).ready(function() {
-  $('.user_question_items_btn').on('click', function() {
-    $('.user_question_items_btn').removeClass('active');
-    $(this).addClass('active');
-  });
-});
+//$(document).ready(function() {
+  //$('.user_question_items_btn').on('click', function() {
+    //$('.user_question_items_btn').removeClass('active');
+    //$(this).addClass('active');
+ // });//
+//});
 
 
 
@@ -20,7 +20,32 @@ $(document).ready(function() {
 $(document).ready(function() {
   var btn = $('.hamburger'),
       content = $('.content'),
-      menu = $('nav');
+      menu = $('nav'),
+      content1 = $('.content-chat'),
+      content2 = $('.content-relationship'),
+      content3 = $('.content-marriage');
+      
+  
+  $('.chat').on('click', function(event) {
+   content1.show();
+   content2.hide();
+   content3.hide();});
+    
+   
+  $('.relationship').on('click', function(event) {
+   content1.hide();
+   content2.show();
+   content3.hide();});
+   
+  
+  $('.marriage').on('click', function(event) {
+   content1.hide();
+   content2.hide();
+   content3.show();});
+   
+  
+ 
+
   $('.hamburger').on('click', function(event) {
     event.preventDefault();
     content.toggleClass('open');
